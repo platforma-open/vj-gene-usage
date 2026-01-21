a7b6c3c57fcadbec08f6674a76f522e2ae835891
Mariia Zueva <maryzueva@gmail.com>
2026-01-21 18:04:28 +0100

    Set default block label

---
+++ .changeset/odd-webs-judge.md
@@ -0,0 +1,5 @@
+---
+"@platforma-open/milaboratories.vj-usage.model": patch
+---
+
+Set default block label
--- model/src/index.ts
+++ model/src/index.ts
@@ -20,7 +20,7 @@ export type UiState = {
 export const model = BlockModel.create()
 
   .withArgs<BlockArgs>({
-    defaultBlockLabel: '',
+    defaultBlockLabel: 'Select Dataset',
     customBlockLabel: '',
     scChain: 'A',
     allele: false,
